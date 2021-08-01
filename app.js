610db0c65ad1d2539f949e04ec6bf2a5ffd42f37
Mahendrareddy190 <73093527+Mahendrareddy190@users.noreply.github.com>
2021-08-01 09:49:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use("/api", studentrouter);
 app.use("/api", Attendencerouter);
 app.use("/api", Authrouter);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
